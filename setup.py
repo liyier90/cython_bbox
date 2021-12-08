a6a8ea01ad095a33224c39e2cf77404147e306e4
yier <56420072+liyier90@users.noreply.github.com>
2021-12-08 16:23:41 +0800

    build: set up build dependencies in pyproject.toml

--- setup.py
+++ setup.py
@@ -7,42 +7,45 @@
 
 from __future__ import print_function
 
-from Cython.Build import cythonize
-from Cython.Distutils import build_ext
-from setuptools import Extension
-from setuptools import setup
+import sys
 
 import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension, setup
 
-
-# Obtain the numpy include directory.  This logic works across numpy versions.
-try:
-    numpy_include = np.get_include()
-except AttributeError:
-    numpy_include = np.get_numpy_include()
+# Obtain the numpy include directory, only for numpy >= 1.0.0
+numpy_include = np.get_include()
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+if sys.platform == "win32":
+    extra_compile_args = ["/Qstd=c99"]
+else:
+    extra_compile_args = ["-std=c99"]
+
 ext_modules = [
     Extension(
-        name='cython_bbox',
-        sources=['src/cython_bbox.pyx'],
-        extra_compile_args = {'gcc': ['/Qstd=c99']},
-        include_dirs=[numpy_include]
+        name="cython_bbox",
+        sources=["src/cython_bbox.pyx"],
+        extra_compile_args=extra_compile_args,
+        include_dirs=[numpy_include],
     )
 ]
 
+compiler_directives = {"language_level": 3}
+ext_modules = cythonize(ext_modules, compiler_directives=compiler_directives)
+
 setup(
-    name='cython_bbox',
-    ext_modules=cythonize(ext_modules),
-    version = '0.1.3',
-    description = 'Standalone cython_bbox',
+    name="cython_bbox",
+    ext_modules=ext_modules,
+    version="0.1.3",
+    description="Standalone cython_bbox",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author = 'Samson Wang',
-    author_email = 'samson.c.wang@gmail.com',
-    url = 'https://github.com/samson-wang/cython_bbox.git', 
-    keywords = ['cython_bbox']
+    author="Samson Wang",
+    author_email="samson.c.wang@gmail.com",
+    url="https://github.com/samson-wang/cython_bbox.git",
+    keywords=["cython_bbox"],
 )
-
